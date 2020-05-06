130db4d17cbbc5b44203089b649016dafa527c4d
skalitin <sergey.kalitin@gmail.com>
2020-05-06 17:51:38 +0300

    Added test for different proto message versions

--- Porcupine.Shared.Tests/SerializationTests.cs
+++ Porcupine.Shared.Tests/SerializationTests.cs
@@ -25,11 +25,45 @@ namespace Porcupine.Shared.Tests
             var response = new CopyFileResponse() { Path = "a" };
             var bytes = Google.Protobuf.MessageExtensions.ToByteArray(response);
 
-            var expected = new byte[] { 10, 1, 97, 18 };
+            var expected = new byte[] { 10, 1, 97 };
             Assert.Equal(expected, bytes);
 
             var deserialized = CopyFileResponse.Parser.ParseFrom(bytes);
             Assert.Equal("a", deserialized.Path);
+        }
+
+        [Fact]
+        public void SerializingCopyFileRequest_v1_to_v2()
+        {
+            var request = new Porcupine.CopyFileRequest() { Source = "source", Target = "target", CopyPermissions = true };
+            var bytes = Google.Protobuf.MessageExtensions.ToByteArray(request);
+
+            var deserialized_v2 = PorcupineTest.CopyFileRequest.Parser.ParseFrom(bytes);
+            Assert.Equal("source", deserialized_v2.Source);
+            Assert.Equal("target", deserialized_v2.Target);
+            Assert.False(deserialized_v2.OverwriteExistingFile);
+            
+            var deserialized_v1 = Porcupine.CopyFileRequest.Parser.ParseFrom(bytes);
+            Assert.Equal("source", deserialized_v1.Source);
+            Assert.Equal("target", deserialized_v1.Target);
+            Assert.True(deserialized_v1.CopyPermissions);
+        }
+
+        [Fact]
+        public void SerializingCopyFileRequest_v2_to_v1()
+        {
+            var request = new PorcupineTest.CopyFileRequest() { Source = "source", Target = "target", OverwriteExistingFile = true };
+            var bytes = Google.Protobuf.MessageExtensions.ToByteArray(request);
+
+            var deserialized_v2 = PorcupineTest.CopyFileRequest.Parser.ParseFrom(bytes);
+            Assert.Equal("source", deserialized_v2.Source);
+            Assert.Equal("target", deserialized_v2.Target);
+            Assert.True(deserialized_v2.OverwriteExistingFile);
+            
+            var deserialized_v1 = Porcupine.CopyFileRequest.Parser.ParseFrom(bytes);
+            Assert.Equal("source", deserialized_v1.Source);
+            Assert.Equal("target", deserialized_v1.Target);
+            Assert.False(deserialized_v1.CopyPermissions);
         }
     }
 }
